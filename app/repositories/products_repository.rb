ebe4479da4ca55253116107cbf02e087c9ef0104
Lucas Medeiros <lucastoc@gmail.com>
2017-10-28 01:35:21 -0300

    add specs for admin reports

--- app/repositories/products_repository.rb
+++ app/repositories/products_repository.rb
@@ -3,27 +3,27 @@
 class ProductsRepository
   delegate :all, :find_by, to: :base_model
 
-  def initialize(base_model: Product, cart_model: Cart)
+  def initialize(base_model: Product)
     @base_model = base_model
-    @cart_model = cart_model
   end
 
   def products_quantities
-    cart_model
-      .joins(:items)
-      .pending
-      .group('items.product_id').sum('items.quantity')
+    Product
+      .left_outer_joins(items: [:cart])
+      .where('carts.status' => Cart.statuses[:pending])
+      .group('items.product_id')
+      .sum('items.quantity')
   end
 
   def products_amounts
-    cart_model
-      .joins(items: [:product])
-      .pending
+    Product
+      .left_outer_joins(items: [:cart])
+      .where('carts.status' => Cart.statuses[:pending])
       .group('items.product_id')
       .sum('(items.quantity * products.price)')
   end
 
   private
 
-  attr_reader :base_model, :cart_model
+  attr_reader :base_model
 end
